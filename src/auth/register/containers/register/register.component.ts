61610c549c6ac0adfdcd9a2c10ae712eb6e93ae3
DeividZavala <crusedmoss@gmail.com>
2019-02-10 12:27:32 -0600

    auth modules configuration

---
+++ src/auth/register/containers/register/register.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "register",
+  template: `
+    <div>
+      Register
+      <auth-form></auth-form>
+    </div>
+  `
+})
+
+export class RegisterComponent {
+  constructor() {
+  }
+}
---
+++ src/auth/shared/containers/auth-form/auth-form.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "auth-form",
+  styleUrls: ["./auth-form.component.scss"],
+  template: `
+    <div>
+      AuthForm
+    </div>
+  `
+})
+
+export class AuthFormComponent {
+  constructor() {
+  }
+}
