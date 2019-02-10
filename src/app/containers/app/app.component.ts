97da8d5f5c7c61786a97e080347c2fc1afaa077b
DeividZavala <crusedmoss@gmail.com>
2019-02-10 15:49:57 -0600

    adding reactive-store

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from "store";
+import {AuthService, User} from "../../../auth/shared/services/auth/auth.service";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      Hello Ultimate Angular!
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy{
+
+  subscription: Subscription;
+  user$: Observable<User>;
+
+  constructor(
+    private authService: AuthService,
+    private store: Store
+  ) {}
+
+  ngOnInit(){
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>("user");
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+
 }
--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,11 +1,33 @@
 import {Injectable} from "@angular/core";
-
+import 'rxjs/add/operator/do';
 import {AngularFireAuth} from "angularfire2/auth";
+import {Store} from "store";
+
+export interface User {
+  email: string,
+  uid: string,
+  authenticated: boolean
+}
 
 @Injectable()
 export class AuthService{
+
+  auth$ = this.af.authState.do(next => {
+    if(!next){
+      return this.store.set("user", null)
+    }
+    const {email, uid} = next;
+    const user: User = {
+      email,
+      uid,
+      authenticated: true
+    };
+    this.store.set("user", user);
+  });
+
   constructor(
-    private af: AngularFireAuth
+    private af: AngularFireAuth,
+    private store: Store
   ){}
 
   createUser(email: string, password: string){
